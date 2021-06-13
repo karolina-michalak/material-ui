87cbcbd0aa6079852c9868583ab41c6ae3747754
Karolina Michalak <karolina0michalak@gmail.com>
2021-06-13 19:48:56 +0200

    makeStyles hook

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Typography } from "@material-ui/core";
+import AcUnitIcon from "@material-ui/icons/AcUnit";
+import { makeStyles } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: "violet",
+    "&:hover": {
+      backgroundColor: "blue",
+    },
+  },
+  title: {
+    textDecoration: "underline",
+    marginBottom: "20px",
+  },
+});
 
 export default function Create() {
+  const classes = useStyles();
+
   return (
     <div>
-      Create page
+      <Typography className={classes.title} color="secondary">
+       hhhhh <AcUnitIcon></AcUnitIcon>
+      </Typography>
+      <Button color="secondary" variant="contained" className={classes.btn}>
+        Submit
+      </Button>
     </div>
-  )
+  );
 }
